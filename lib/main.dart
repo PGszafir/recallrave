01a58e5058a8ed2d16a4e5f94780e61ab6c1573b
PGszafir <pgszafir@gmail.com>
2023-12-17 23:23:11 +0100

    productive comments

--- lib/main.dart
+++ lib/main.dart
@@ -53,14 +53,16 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Center(
+          child: Text(widget.title),
+        )
       ),
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            for (final product in products)
-              ListTile(
+            for (final product in products)// iterate for product list and add product viev
+              ListTile(// replace this with the product viev
                 title: Text(product['name']),
                 subtitle: Text(product['manufacturer']),
                 leading: Image.asset(product['image']),
@@ -81,4 +83,6 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+// ad here or in other file product viev
 
